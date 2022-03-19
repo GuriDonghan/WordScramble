57a4c631e76adb5a615d8a4f15cd430ba8a7a7c0
GuriDonghan <snuffy@naver.com>
2022-03-19 20:34:01 +0900

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by USER on 2022/03/19.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
